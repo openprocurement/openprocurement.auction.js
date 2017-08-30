886c630ed8464e0409340a2610bddae22cc2a517
Yaroslav Shalenyk <yshalenyk@quinatagroup.org>
2017-08-30 14:52:43 +0300

    Fixed setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(name='openprocurement.auction.js',
+      version='1.0.1a1+eauctions',
+      description='frontend application for openprocurement.auction',
+      classifiers=[
+          "Framework :: Pylons",
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application"
+      ],
+      keywords="web services",
+      author='Quintagroup, Ltd.',
+      author_email='info@quintagroup.com',
+      license='Apache License 2.0',
+      url='https://github.com/yshalenyk/openprocurement.auction.js',
+      include_package_data=True,
+      zip_safe=False,
+)
