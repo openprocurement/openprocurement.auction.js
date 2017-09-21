59691260025e15dd8647a35df48f03661e22fb7f
Yuriy Hvozdovych <gvizdyk@quintagroup.com>
2017-09-21 20:50:30 +0300

    fix gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp          = require('gulp'),
       notify        = require('gulp-notify'),
       del           = require('del'),
       concat        = require('gulp-concat'),
+      util          = require('gulp-util'),
       vendorFiles   = require('gulp-main-bower-files'),
       minify        = require('gulp-minify'),
       gulpFilter    = require('gulp-filter'),
@@ -25,6 +26,10 @@ function  interceptErrors(error) {
 
 
 const config = JSON.parse(fs.readFileSync('./config.json'));
+const db_name = config.db_name || 'database';
+const app_name = config.app_name || 'auction.js';
+const devel = ('devel' in config) ? config.devel : true;
+const main_css = config.main_css || 'bundle.css';
 
 
 gulp.task('fonts', () => {
@@ -47,27 +52,27 @@ gulp.task('icons', () => {
     .pipe(gulp.dest(config.buildDir+'/img/'));
 });
 
-
 gulp.task('bower-main', () => {
   return allJs = gulp.src('./bower.json')
-      .pipe(vendorFiles({
-        base: "src/lib",}))
-      .pipe(gulpFilter(['**/*.js']))
-      .pipe(gulp.dest(config.buildDir + '/vendor/'));
+    .pipe(vendorFiles({base: "src/lib"}))
+    .pipe(gulpFilter(['**/*.js']))
+    .pipe(gulp.dest(config.buildDir + '/vendor/'));
 });
 
-
 gulp.task('all-js', ['bower-main'], () => {
+  // TODO: uglify only on debug == false
   return gulp.src([
     config.buildDir + '/vendor/angular/angular.min.js',
     config.buildDir + '/vendor/**/**/*.js',
     './src/lib/moment/locale/uk.js',
     './src/lib/moment/locale/ru.js',
-    './src/lib/puchdb/**/*.js',
-  ])
-    .pipe(concat('vendor.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest(config.buildDir));
+    './src/lib/puchdb/**/*.js'])
+  .pipe(concat('vendor.js'))
+  .pipe(devel ? util.noop() : uglify())
+  .pipe(gulp.dest(config.buildDir))
+  .on('end', () => {
+    del([config.buildDir + '/vendor'])
+  });
 });
 
 
@@ -89,8 +94,9 @@ gulp.task('htmlPages', () => {
         title: page.title,
         name: page.name,
         scripts: page.scripts,
-	controller: page.controller,
+        controller: page.controller,
         db_url: config.dbUrl,
+        db_name: db_name,
         auctions_server: config.auctions_server
       }}))
     .on('error', interceptErrors)
@@ -106,7 +112,7 @@ gulp.task('listingApp', () => {
     './src/app/config.js',
     './src/app/controllers/ListingCtrl.js'
     ])
-    .pipe(concat('index_app_simple.js'))
+    .pipe(concat('index.js'))
     .pipe(gulp.dest(config.buildDir));
 });
 
@@ -115,34 +121,35 @@ gulp.task('archiveApp', () => {
   return gulp.src(['./src/app/archive.js',
     './src/app/config.js',
     './src/app/controllers/ArchiveCtl.js'])
-    .pipe(concat('archive_app_simple.js'))
+    .pipe(concat('archive.js'))
     .pipe(gulp.dest(config.buildDir));
 });
 
 
 gulp.task('auctionApp', () => {
-    return gulp.src(['./src/app/auction.js',
-      './src/app/filters/*.js',
-      './src/app/translations.js',
-      './src/app/config.js',
-      './src/app/factories/*.js',
-      './src/app/controllers/AuctionCtl.js',
-      './src/app/controllers/OffCanvasCtl.js',
-      './src/app/directives/*.js'])
-    .pipe(concat('auction_app_simple.js'))
+  return gulp.src(['./src/app/auction.js',
+    './src/app/filters/*.js',
+    './src/app/translations.js',
+    './src/app/config.js',
+    './src/app/factories/*.js',
+    './src/app/controllers/AuctionCtl.js',
+    './src/app/controllers/OffCanvasCtl.js',
+    './src/app/directives/*.js'])
+    .pipe(concat(app_name))
+    .pipe(devel ? util.noop() : uglify({ mangle: false}))
     .pipe(gulp.dest(config.buildDir));
 });
 
 
 gulp.task('build', ['all-js', 'css', 'png-images', 'icons', 'htmlPages', 'listingApp', 'archiveApp', 'auctionApp', 'fonts'], () => {
 
-  let css = gulp.src(`${config.buildDir}/bundle.css`)
+  let css = gulp.src(`${config.buildDir}/${main_css}`)
       .pipe(gulp.dest(config.outDir + '/static/css/'));
 
   let listPage = gulp.src(`${config.buildDir}/index.html`)
       .pipe(gulp.dest(config.outDir));
 
-  let listApp = gulp.src(`${config.buildDir}/index_app_simple.js`)
+  let listApp = gulp.src(`${config.buildDir}/index.js`)
       .pipe(gulp.dest(config.outDir + '/static/'));
 
   let vendor_js = gulp.src(`${config.buildDir}/vendor.js`)
@@ -151,13 +158,13 @@ gulp.task('build', ['all-js', 'css', 'png-images', 'icons', 'htmlPages', 'listin
   let archivePage = gulp.src(`${config.buildDir}/archive.html`)
       .pipe(gulp.dest(config.outDir));
 
-  let archiveApp = gulp.src(`${config.buildDir}/archive_app_simple.js`)
+  let archiveApp = gulp.src(`${config.buildDir}/archive.js`)
       .pipe(gulp.dest(config.outDir + '/static/'));
 
   let auctionPage = gulp.src(`${config.buildDir}/tender.html`)
       .pipe(gulp.dest(config.outDir));
 
-  let auctionApp = gulp.src(`${config.buildDir}/auction_app_simple.js`)
+  let auctionApp = gulp.src(`${config.buildDir}/${app_name}`)
       .pipe(gulp.dest(config.outDir + '/static/'));
 
   let png = gulp.src("build/*.png")
@@ -168,16 +175,16 @@ gulp.task('build', ['all-js', 'css', 'png-images', 'icons', 'htmlPages', 'listin
 
   let fonts = gulp.src("build/fonts/*")
       .pipe(gulp.dest(config.outDir+'/static/fonts/'));
- 
+
   let fonts2 = gulp.src("build/fonts/*")
       .pipe(gulp.dest(config.outDir+'/fonts/'));
 
 
-  return merge(css, png, fonts, vendor_js, listPage, listApp, auctionPage, auctionApp, archivePage, archiveApp, fonts, fonts2, icons);
+  return merge(css, png, vendor_js, listPage, listApp, auctionPage, auctionApp, archivePage, archiveApp, fonts, fonts2, icons);
 });
 
 
-gulp.task('default', ['build']);
+gulp.task('default', ['clean', 'build']);
 
 gulp.task('clean', function () {
   del.sync([config.buildDir + '*/**', config.outDir + '*/**'], {force: true});
