7a08200954d5ec03d63c1569214da876705cf6a6
Yaroslav Neznaradko <nezn27@gmail.com>
2017-09-29 15:30:08 +0300

    Update index.js

--- src/app/index.js
+++ src/app/index.js
@@ -5,5 +5,5 @@ const requires = [
 
 
 angular.module('auction', requires).constant(
-    'AuctionConfig', { db_url:  (location.protocol + '//' + location.host + '/auctions/' ) || ""
+    'AuctionConfig', { db_url:  (location.protocol + '//' + location.host + '/database/' ) || ""
 })
