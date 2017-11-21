44160b1c3969695f537f64ed4098c521e10860ab
Yuriy Hvozdovych <gvizdyk@quintagroup.com>
2017-11-21 18:55:16 +0200

    Resolved a483545624707730. Updated translations.

--- src/app/translations.js
+++ src/app/translations.js
@@ -89,7 +89,9 @@ angular.module('auction')
       'The proposal you have submitted coincides with a proposal of the other participant. His proposal will be considered first, since it has been submitted earlier.': 'The proposal you have submitted coincides with a proposal of the other participant. His proposal will be considered first, since it has been submitted earlier.',
       'Waiting for the disclosure of the participants\' names': 'Waiting for the disclosure of the participants\' names',
       'Unable to place a bid. Check that no more than 2 auctions are simultaneously opened in your browser.': 'Unable to place a bid. Check that no more than 2 auctions are simultaneously opened in your browser.',
-      'Please use the correct link to view the auction.': 'Please use the correct link to view the auction.'
+      'Please use the correct link to view the auction.': 'Please use the correct link to view the auction.',
+      'Auction has not started and will be rescheduled': 'Auction has not started and will be rescheduled',
+      'Please wait for the auction start.': 'Please wait for the auction start.'
     });
 
     $translateProvider.translations('uk', {
@@ -179,7 +181,9 @@ angular.module('auction')
       'The proposal you have submitted coincides with a proposal of the other participant. His proposal will be considered first, since it has been submitted earlier.': 'Подана вами пропозиція співпадає з пропозицією іншого учасника. Його пропозиція розглядатиметься першою, оскільки вона подана раніше.',
       'Waiting for the disclosure of the participants\' names': 'Очікуємо на розкриття імен учасників',
       'Unable to place a bid. Check that no more than 2 auctions are simultaneously opened in your browser.': 'Не вдається зробити ставку. Перевірте, що в переглядачі відкрито не більше 2-ох аукціонів.',
-      'Please use the correct link to view the auction.': 'Будь ласка, використовуйте правильне посилання для перегляду аукціону.'
+      'Please use the correct link to view the auction.': 'Будь ласка, використовуйте правильне посилання для перегляду аукціону.',
+      'Auction has not started and will be rescheduled': 'Аукціон не розпочався і буде перепланований',
+      'Please wait for the auction start.': 'Будь ласка, дочекайтесь початку аукціону.'
     });
 
 
@@ -269,6 +273,8 @@ angular.module('auction')
       'The proposal you have submitted coincides with a proposal of the other participant. His proposal will be considered first, since it has been submitted earlier.': 'Поданное вами предложение совпадает с предложением другого участника. Его предложение будет рассматриваться первым, поскольку оно подано раньше.',
       'Waiting for the disclosure of the participants\' names': 'Ожидаем раскрытия имен участников',
       'Unable to place a bid. Check that no more than 2 auctions are simultaneously opened in your browser.': 'Невозможно сделать ставку. Проверьте, что в браузере открыто не более 2-х аукционов.',
-      'Please use the correct link to view the auction.': 'Пожалуйста, используйте правильную ссылку для просмотра аукциона.'
+      'Please use the correct link to view the auction.': 'Пожалуйста, используйте правильную ссылку для просмотра аукциона.',
+      'Auction has not started and will be rescheduled': 'Аукцион не начался и будет перепланирован',
+      'Please wait for the auction start.': 'Пожалуйста, дождитесь начала аукциона.'
     });
   }]);
