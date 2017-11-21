f1796ffb6a5442de68ab606a71eb5e2a84600e1a
Yuriy Hvozdovych <gvizdyk@quintagroup.com>
2017-11-21 19:43:58 +0200

    Resolved a484777993214377. Update configs, build development process.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,26 @@
 const gulp = require('gulp'),
   notify = require('gulp-notify'),
   del = require('del'),
-  path = require('path'),
   concat = require('gulp-concat'),
   util = require('gulp-util'),
+  vendorFiles = require('gulp-main-bower-files'),
   minify = require('gulp-minify'),
   gulpFilter = require('gulp-filter'),
   source = require('vinyl-source-stream'),
   cleanCSS = require('gulp-clean-css'),
   fileinclude = require('gulp-file-include'),
   uglify = require('gulp-uglify'),
-  rename = require("gulp-rename"),
-  fs = require("fs"),
+  rename = require('gulp-rename'),
+  fs = require('fs'),
+  less = require('gulp-less'),
+  lessAutoprefix = require('less-plugin-autoprefix'),
   merge = require('merge-stream'),
-  eslint = require('gulp-eslint'),
-  pack = require('gulp-tar'),
-  gzip = require('gulp-gzip'),
-  sequence = require('run-sequence'),
-  server = require('karma').Server;
-
+  sourcemaps = require('gulp-sourcemaps'),
+  render = require('gulp-nunjucks-render'),
+  data = require('gulp-data'),
+  rev = require('gulp-rev'),
+  revReplace = require('gulp-rev-replace'),
+  revdel = require('gulp-rev-delete-original');
 
 function interceptErrors(error) {
   let args = Array.prototype.slice.call(arguments);
@@ -29,170 +31,95 @@ function interceptErrors(error) {
   this.emit('end');
 }
 
-
 const config = JSON.parse(fs.readFileSync('./config.json'));
-const db_name = config.db_name || 'database';
-const app_name = config.app_name || 'auction.js';
 const devel = ('devel' in config) ? config.devel : true;
-const main_css = config.main_css || 'bundle.css';
-const name = config.name || 'tender';
-const staticRoot = 'static'
-const fontDir = `${staticRoot}/fonts/`;
-const imgDir = `${staticRoot}/img/`;
-const cssDir = `${staticRoot}/css/`;
-const packageName = 'openprocurement.auction.js';
-const tmpDir = '/tmp/insider';
-const buildDeps = ['css', 'png-images', 'icons', 'htmlPages', 'auctionApp', 'fonts', 'archiveApp', 'listingApp'];
 
-
-gulp.task('fonts', () => {
-  let dest = path.join(config.buildDir, fontDir);
-  return gulp.src(config.fonts)
-    .on('error', interceptErrors)
-    .pipe(gulp.dest(dest));
+gulp.task('base:all', () => {
+  return gulp.src(config.assets).pipe(gulp.dest(config.buildDir));
 });
 
-
-gulp.task('png-images', () => {
-  let dest = path.join(config.buildDir, imgDir);
-  return gulp.src(config.img.png)
-    .on('error', interceptErrors)
-    .pipe(gulp.dest(dest));
-});
-
-
-gulp.task('icons', () => {
-  let dest = path.join(config.buildDir, imgDir);
-  return gulp.src(config.img.icons)
-    .on('error', interceptErrors)
-    .pipe(gulp.dest(dest));
-});
-
-
-gulp.task('all-js', () => {
-  let dest = path.join(config.buildDir, staticRoot);
-  console.log(dest)
-  return gulp.src(config.packages)
+gulp.task('js:vendor', () => {
+  return gulp.src(config.js)
+    .pipe(sourcemaps.init())
     .pipe(concat('vendor.js'))
-    .pipe(devel ? util.noop() : uglify({
-      mangle: false
-    }))
-    .pipe(gulp.dest(dest))
-    .on('error', util.log);
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(config.buildDir + '/static/js'));
 });
 
-
-gulp.task('css', () => {
-  let dest = path.join(config.buildDir, cssDir);
-  return gulp.src(config.styles)
-    .pipe(concat(main_css))
-    .pipe(cleanCSS())
-    .on('error', interceptErrors)
-    .pipe(gulp.dest(dest));
+gulp.task('js:tenders', () => {
+  return gulp.src(config.modules.tenders.js)
+    .pipe(sourcemaps.init())
+    .pipe(concat('tenders.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(config.buildDir + '/static/js'));
 });
 
-
-gulp.task('htmlPages', () => {
-  return merge(config.html.map((page) => {
-    return gulp.src('./templates/base.html')
-      .pipe(fileinclude({
-        prefix: '@@',
-        indent: true,
-        context: {
-          title: page.title,
-          name: page.name,
-          scripts: page.scripts,
-          styles: page.styles,
-          controller: page.controller,
-          db_name: db_name,
-          auctions_server: config.auctions_server
-        }
-      }))
-      .on('error', interceptErrors)
-      .pipe(rename(page.name + '.html'))
-      .pipe(gulp.dest(config.buildDir));
-  }));
-});
-
-
-gulp.task('listingApp', () => {
-  return gulp.src(config.listingApp)
+gulp.task('js:index', () => {
+  return gulp.src(config.modules.index.js)
     .pipe(concat('index.js'))
-    .pipe(devel ? util.noop() : uglify({
-      mangle: false
-    }))
-    .pipe(gulp.dest(path.join(config.buildDir, staticRoot)));
+    .pipe(gulp.dest(config.buildDir + '/static/js'));
 });
 
-
-gulp.task('archiveApp', () => {
-  return gulp.src(config.archiveApp)
+gulp.task('js:archive', () => {
+  return gulp.src(config.modules.archive.js)
     .pipe(concat('archive.js'))
-    .pipe(devel ? util.noop() : uglify({
-      mangle: false
+    .pipe(gulp.dest(config.buildDir + '/static/js'));
+});
+
+gulp.task('css:less', () => {
+  let autoprefix = new lessAutoprefix({ browsers: ['last 2 versions'] });
+  return gulp.src('src/assets/css/starter-template.less')
+    .pipe(less({
+      plugins: [autoprefix]
     }))
-    .pipe(gulp.dest(path.join(config.buildDir, staticRoot)));
+    .on('error', interceptErrors)
+    .pipe(gulp.dest('src/assets/css/'));
 });
 
+gulp.task('css:all', ['css:less'], () => {
+  return gulp.src(config.styles)
+    .pipe(concat('all.css'))
+    .pipe(cleanCSS({ compatibility: 'ie11' }))
+    .pipe(gulp.dest(config.buildDir + '/static/css'));
+});
 
-gulp.task('auctionApp', () => {
-  return gulp.src(config.auctionApp)
-    .pipe(concat(app_name))
-    .pipe(devel ? util.noop() : uglify({
-      mangle: false
+gulp.task('html:all', () => {
+  return gulp.src('templates/*.html')
+    .pipe(render({
+      path: 'templates/',
+      data: config,
     }))
-    .on("error", interceptErrors)
-    .pipe(gulp.dest(path.join(config.buildDir, staticRoot)));
+    .pipe(gulp.dest(config.buildDir));
 });
 
+gulp.task('revision', ['base:all', 'js:vendor', 'js:tenders', 'js:index', 'js:archive', 'css:all', 'html:all'], function() {
+  return gulp.src([config.buildDir + '/**/*.css', config.buildDir + '/**/*.js'])
+    .pipe(rev())
+    .pipe(gulp.dest(config.buildDir))
+    .pipe(revdel())
+    .pipe(rev.manifest())
+    .pipe(gulp.dest(config.buildDir));
+})
 
-gulp.task('pack', () => {
-  return gulp.src([`${config.buildDir}/**/*`])
-    .pipe(pack(`${packageName}.tar`))
-    .pipe(gzip())
-    .pipe(gulp.dest('dist'))
-    .on("error", interceptErrors)
+gulp.task('revreplace', ['revision'], function() {
+  let manifest = gulp.src(config.buildDir + '/rev-manifest.json');
+  return gulp.src(config.buildDir + '/*.html')
+    .pipe(revReplace({ manifest: manifest }))
+    .pipe(gulp.dest(config.buildDir));
 });
 
-
-gulp.task('lint', () => {
-  return gulp.src(config.appUtils)
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
-
-
-gulp.task('clean', function () {
-  del.sync([config.buildDir + '*/**', config.outDir + '*/**'], {
-    force: true
-  });
-});
-
-
-gulp.task('bundle', buildDeps);
-
-
-gulp.task('copyToDest', () => {
-  return gulp.src([`${config.buildDir}/**/*`])
-    .on("error", interceptErrors)
+gulp.task('build', ['revreplace'], () => {
+  return gulp.src(config.buildDir + '/**/*.*')
     .pipe(gulp.dest(config.outDir));
 });
 
+gulp.task('build:buildout', ['clean', 'build'], () => {
+  return gulp.src(config.outDir + '/**/*.*')
+    .pipe(gulp.dest(config.buildout_outDir));
+})
 
-gulp.task('build', (done) => {
-  return sequence('all-js', 'bundle', 'copyToDest', () => {
-    done();
-  });
-});
+gulp.task('default', ['build:buildout']);
 
-
-gulp.task('default', ['build']);
-
-
-gulp.task('test', function (done) {
-  new server({
-    configFile: __dirname + '/karma.conf.js',
-    singleRun: true
-  }, done).start();
+gulp.task('clean', function() {
+  del.sync([config.buildDir + '*/**', config.outDir + '*/**'], { force: true });
 });
