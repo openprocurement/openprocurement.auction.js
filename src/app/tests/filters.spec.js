49806d0317a992826bc4ed041461f12f54b22d78
Yuriy Hvozdovych <gvizdyk@quintagroup.com>
2017-11-24 18:55:22 +0200

    up spaces in the js tests

--- src/app/tests/filters.spec.js
+++ src/app/tests/filters.spec.js
@@ -10,7 +10,6 @@ describe('Unit: Testing Filter "formatnumber" - ', function() {
     expect($filter('formatnumber')(10)).toEqual('10');
     expect($filter('formatnumber')(1)).toEqual('1');
   }));
-
   it('formatnumber should format nagative numbers', inject(function($filter) {
     expect($filter('formatnumber')(-100000)).toEqual('-100 000');
     expect($filter('formatnumber')(-10000)).toEqual('-10 000');
@@ -19,10 +18,9 @@ describe('Unit: Testing Filter "formatnumber" - ', function() {
     expect($filter('formatnumber')(-10)).toEqual('-10');
     expect($filter('formatnumber')(-1)).toEqual('-1');
   }));
-
   it('formatnumber should works with incorect values', inject(function($filter) {
     expect($filter('formatnumber')('string')).toEqual('');
-    expect($filter('formatnumber')('')).toEqual($filter('number')('')); // '0' 
+    expect($filter('formatnumber')('')).toEqual($filter('number')('')); // '0'
     expect($filter('formatnumber')({})).toEqual('');
     expect($filter('formatnumber')(true)).toEqual('1');
     expect($filter('formatnumber')(false)).toEqual('0');
