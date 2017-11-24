07f40e4fafd4ceb0e1e7e673836996f076c8516b
Yuriy Hvozdovych <gvizdyk@quintagroup.com>
2017-11-24 19:07:05 +0200

    Fixed unit js tests.

--- src/app/tests/controllers.spec.js
+++ src/app/tests/controllers.spec.js
@@ -1,148 +1,148 @@
 describe('auctionTest', function() {
   beforeEach(module('auction'));
-  var controller, scope, AuctionUtils;
+  var controller, AuctionUtils, rootScope;
   beforeEach(inject(function(_$controller_, _$rootScope_, AuctionUtils) {
-    scope = _$rootScope_.$new();
-    controller = _$controller_('AuctionController', { $scope: scope, AuctionUtils: AuctionUtils });
+    rootScope = _$rootScope_.$new();
+    controller = _$controller_('AuctionController', { AuctionUtils: AuctionUtils, $rootScope: rootScope });
   }));
 
-  //$scope.post_bid
+  //$rootScope.post_bid
   it('should be Defined', function() {
-    expect(scope.post_bid).toBeDefined();
+    expect(rootScope.post_bid).toBeDefined();
   });
   it('should push alerts', function() {
-    let length = scope.alerts.length;
-    scope.form.bid = -1;
-    expect(!!scope.post_bid()).toEqual(false);
-    expect(scope.alerts.length).toEqual(length + 1);
+    let length = rootScope.alerts.length;
+    rootScope.form.bid = -1;
+    expect(!!rootScope.post_bid()).toEqual(false);
+    expect(rootScope.alerts.length).toEqual(length + 1);
   });
   it('should warn if the proposal you have submitted coincides with a proposal of the other participant bid', function() {
-    let length = scope.alerts.length;
-    scope.form.BidsForm = {};
-    scope.form.BidsForm.$valid = true;
-    scope.minimal_bid = { amount: 2 };
-    scope.post_bid(2);
-    expect(scope.alerts.length).toEqual(length + 1);
-    expect(scope.alerts[0].msg && scope.alerts[0].type == 'warning').toEqual(true);
+    let length = rootScope.alerts.length;
+    rootScope.form.BidsForm = {};
+    rootScope.form.BidsForm.$valid = true;
+    rootScope.minimal_bid = { amount: 2 };
+    rootScope.post_bid(2);
+    expect(rootScope.alerts.length).toEqual(length + 1);
+    expect(rootScope.alerts[0].msg && rootScope.alerts[0].type == 'warning').toEqual(true);
   });
 
-  //$scope.edit_bid
+  //$rootScope.edit_bid
   it('should be Defined', function() {
-    expect(scope.edit_bid).toBeDefined();
+    expect(rootScope.edit_bid).toBeDefined();
   });
   it('should change allow_bidding', function() {
-    scope.edit_bid();
-    expect(scope.allow_bidding).toBe(true);
+    rootScope.edit_bid();
+    expect(rootScope.allow_bidding).toBe(true);
   });
 
-  //$scope.max_bid_amount
+  //$rootScope.max_bid_amount
   it('should be Defined', function() {
-    expect(scope.max_bid_amount).toBeDefined();
+    expect(rootScope.max_bid_amount).toBeDefined();
   });
   it('should return 0', function() {
-    expect(scope.max_bid_amount()).toBe(0);
+    expect(rootScope.max_bid_amount()).toBe(0);
   });
   it('should find max bid amount', function() {
-    scope.bidder_id = '{}';
-    scope.auction_doc = { current_stage: 0 };
-    scope.auction_doc.stages = [{ amount: 3 }];
-    scope.auction_doc.minimalStep = { amount: 1 }
-    expect(scope.max_bid_amount()).toBe(2);
+    rootScope.bidder_id = '{}';
+    rootScope.auction_doc = { current_stage: 0 };
+    rootScope.auction_doc.stages = [{ amount: 3 }];
+    rootScope.auction_doc.minimalStep = { amount: 1 }
+    expect(rootScope.max_bid_amount()).toBe(4);
   });
 
-  //$scope.calculate_minimal_bid_amount
+  //$rootScope.calculate_minimal_bid_amount
   it('should be Defined', function() {
-    expect(scope.calculate_minimal_bid_amount).toBeDefined();
+    expect(rootScope.calculate_minimal_bid_amount).toBeDefined();
   });
   it('should find minimal bid', function() {
-    scope.auction_doc = {};
-    scope.auction_doc.stages = [];
-    scope.auction_doc.initial_bids = [{ amount: 1 }, { amount: 2 }, { amount: 3 }];
-    scope.calculate_minimal_bid_amount();
-    expect(scope.minimal_bid).toBeDefined(1);
-    scope.auction_doc.initial_bids = [{ amount: 100000 }, { amount: 200000 }, { amount: 300000 }, { amount: 200000 }, { amount: 300000 }, { amount: 300000 }, { amount: 200000 }, { amount: 300000 }];
-    expect(scope.minimal_bid).toBeDefined(100000);
+    rootScope.auction_doc = {};
+    rootScope.auction_doc.stages = [];
+    rootScope.auction_doc.initial_bids = [{ amount: 1 }, { amount: 2 }, { amount: 3 }];
+    rootScope.calculate_minimal_bid_amount();
+    expect(rootScope.minimal_bid).toBeDefined(1);
+    rootScope.auction_doc.initial_bids = [{ amount: 100000 }, { amount: 200000 }, { amount: 300000 }, { amount: 200000 }, { amount: 300000 }, { amount: 300000 }, { amount: 200000 }, { amount: 300000 }];
+    expect(rootScope.minimal_bid).toBeDefined(100000);
   });
 
-  //$scope.start_sync
+  //$rootScope.start_sync
   it('should be Defined', function() {
-    expect(scope.start_sync).toBeDefined();
+    expect(rootScope.start_sync).toBeDefined();
   });
 
-  //$scope.start_auction_process
+  //$rootScope.start_auction_process
   it('should be Defined', function() {
-    expect(scope.start_auction_process).toBeDefined();
+    expect(rootScope.start_auction_process).toBeDefined();
   });
 
-  //$scope.restart_changes
+  //$rootScope.restart_changes
   it('should be Defined', function() {
-    expect(scope.restart_changes).toBeDefined();
+    expect(rootScope.restart_changes).toBeDefined();
   });
 
-  //$scope.replace_document
+  //$rootScope.replace_document
   it('should be Defined', function() {
-    expect(scope.replace_document).toBeDefined();
+    expect(rootScope.replace_document).toBeDefined();
   });
 
-  //$scope.calculate_rounds
+  //$rootScope.calculate_rounds
   it('should be Defined', function() {
-    expect(scope.calculate_rounds).toBeDefined();
+    expect(rootScope.calculate_rounds).toBeDefined();
   });
   it('should work correct', function() {
-    scope.auction_doc = { stages: [{ type: 'pause' }, { type: 'pause' }, 5, { type: 'pause' }, { type: 'pause' }, { type: 'pause' }] };
-    scope.calculate_rounds();
-    expect(scope.Rounds).toEqual([0, 1, 3, 4, 5]);
+    rootScope.auction_doc = { stages: [{ type: 'pause' }, { type: 'pause' }, 5, { type: 'pause' }, { type: 'pause' }, { type: 'pause' }] };
+    rootScope.calculate_rounds();
+    expect(rootScope.Rounds).toEqual([0, 1, 3, 4, 5]);
   });
 
-  //$scope.scroll_to_stage
+  //$rootScope.scroll_to_stage
   it('should be Defined', function() {
-    expect(scope.scroll_to_stage).toBeDefined();
+    expect(rootScope.scroll_to_stage).toBeDefined();
   });
 
-  //$scope.array
+  //$rootScope.array
   it('should be Defined', function() {
-    expect(scope.array).toBeDefined();
+    expect(rootScope.array).toBeDefined();
   });
   it('should return Array', function() {
-    expect(scope.array() instanceof Array).toBe(true);
+    expect(rootScope.array() instanceof Array).toBe(true);
   });
 
-  //$scope.open_menu
+  //$rootScope.open_menu
   it('should be Defined', function() {
-    expect(scope.open_menu).toBeDefined();
+    expect(rootScope.open_menu).toBeDefined();
   });
 
-  //$scope.calculate_bid_temp
+  //$rootScope.calculate_bid_temp
   it('should be Defined', function() {
-    expect(scope.calculate_bid_temp).toBeDefined();
+    expect(rootScope.calculate_bid_temp).toBeDefined();
   });
   it('should be Defined', function() {
-    scope.form.bid = 11;
-    scope.bidder_coeficient = 4;
-    scope.calculate_bid_temp();
-    expect(scope.form.bid_temp).toBe(11);
-    expect(scope.form.full_price).toBe(2.75);
+    rootScope.form.bid = 11;
+    rootScope.bidder_coeficient = 4;
+    rootScope.calculate_bid_temp();
+    expect(rootScope.form.bid_temp).toBe(11);
+    expect(rootScope.form.full_price).toBe(44);
   });
 
-  //$scope.calculate_full_price_temp
+  //$rootScope.calculate_full_price_temp
   it('should be Defined', function() {
-    expect(scope.calculate_full_price_temp).toBeDefined();
+    expect(rootScope.calculate_full_price_temp).toBeDefined();
   });
   it('should work correct', function() {
-    scope.form.full_price = 11;
-    scope.bidder_coeficient = 3;
-    scope.calculate_full_price_temp();
-    expect(scope.form.full_price_temp).toBe(11);
-    expect(scope.form.bid).toBe(33);
+    rootScope.form.full_price = 11;
+    rootScope.bidder_coeficient = 3;
+    rootScope.calculate_full_price_temp();
+    expect(rootScope.form.full_price_temp).toBe(99);
+    expect(rootScope.form.bid).toBe(33);
   });
 
-  //$scope.set_bid_from_temp
+  //$rootScope.set_bid_from_temp
   it('should be Defined', function() {
-    expect(scope.set_bid_from_temp).toBeDefined();
+    expect(rootScope.set_bid_from_temp).toBeDefined();
   });
   it('should work correct', function() {
-    scope.form.bid_temp = 0;
-    scope.set_bid_from_temp();
-    expect(scope.form.bid).toBe(0);
+    rootScope.form.bid_temp = 0;
+    rootScope.set_bid_from_temp();
+    expect(rootScope.form.bid).toBe(0);
   });
 });
