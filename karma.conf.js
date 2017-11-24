9dbc1e51e2fae040aa58b9d99048a1237f46501b
Yuriy Hvozdovych <gvizdyk@quintagroup.com>
2017-11-24 18:47:57 +0200

    Up pathes in karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -7,31 +7,26 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
 
-
     // list of files / patterns to load in the browser
     files: [
-      './src/lib/angular/angular.js',
-      './src/lib/event-source-polyfill/eventsource.js',
-      './src/lib/pouchdb/dist/pouchdb.min.js',
-      './src/lib/angular-mocks/angular-mocks.js',
-      //'./src/lib/pouchdb/dist/pouchdb.localstorage.js',
-      './src/lib/angular-cookies/angular-cookies.js',
-      './src/lib/angular-translate/*.js',
-      './src/lib/angular-translate-storage-cookie/*.js',
-      './src/lib/angular-translate-storage-local/*.js',
-      './src/lib/angular-timer/dist/*.js',
-      './src/lib/angular-bootstrap/ui-bootstrap.js',
-      './src/lib/angular-growl-2/build/*.js',
-      './src/lib/angular-ellipses/src/*.js',
-      './src/lib/angular-gtm-logger/*.js',
-      './src/lib/mathjs/dist/*.js',
-      //'./src/lib/moment/*.js',
-      //'./src/lib/moment-timezone/*.js',
+      './node_modules/angular/angular.js',
+      './node_modules/event-source-polyfill/eventsource.js',
+      './node_modules/pouchdb/dist/pouchdb.min.js',
+      './node_modules/angular-mocks/angular-mocks.js',
+      './node_modules/angular-cookies/angular-cookies.js',
+      './node_modules/angular-translate/dist/*.js',
+      './node_modules/angular-translate-storage-cookie/*.js',
+      './node_modules/angular-translate-storage-local/*.js',
+      './node_modules/angular-timer/dist/*.js',
+      './node_modules/angular-ui-bootstrap/dist/*.js',
+      './node_modules/angular-growl-v2/build/*.js',
+      './node_modules/angular-ellipses/src/*.js',
+      './node_modules/angular-gtm-logger/*.js',
+      './node_modules/mathjs/dist/*.js',
       './src/app/auction.js',
       './src/app/translations.js',
       './src/app/config.js',
@@ -39,7 +34,6 @@ module.exports = function(config) {
       './src/app/controllers/*.js',
       './src/app/filters/*.js',
       './src/app/directives/*.js',
-      //'test-main.js',
       './src/app/tests/auction_data_samples.js',
       './src/app/tests/auction_utils.spec.js',
       './src/app/tests/controllers.spec.js',
@@ -47,45 +41,34 @@ module.exports = function(config) {
       './src/app/tests/utils.spec.js',
     ],
 
-
     // list of files to exclude
-    exclude: [
-    ],
-
+    exclude: [],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
-
+    preprocessors: {},
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
 
-
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
-
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
