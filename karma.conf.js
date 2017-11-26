d38f7557bfd867be77de5d10c3072dcedeb1c43e
Yuriy Hvozdovych <gvizdyk@quintagroup.com>
2017-11-26 14:45:31 +0200

    configured covarage for js unit test

--- karma.conf.js
+++ karma.conf.js
@@ -46,12 +46,14 @@ module.exports = function(config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {},
+    preprocessors: {
+      'src/**/*.js': ['coverage']
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
     // web server port
     port: 9876,
@@ -76,6 +78,12 @@ module.exports = function(config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
+    concurrency: Infinity,
+
+    // optionally, configure the reporter
+    coverageReporter: {
+      type: 'text'
+    }
+
   })
 }
